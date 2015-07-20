f8ce2eca117729a8af737a45dec34a2374495ac1
Benjamin Toll <benjam72@yahoo.com>
2015-07-20 00:51:21 -0400

    Abstracted the list items that make up the main menu nav to app.json where each one is a list element.

--- NOTES.md
+++ NOTES.md
@@ -2,3 +2,10 @@
 // For the app, we need Pete + app/app.js
 // For the template.html (each quiz sandbox), we just need Pete.
 
+To add a new link to the main quiz menu on index.html, simply add a new list element in `app.json`.
+For example, adding `chord_builder` will build the following link in `app/app.js`:
+
+        <a href="#chord_builder" title="Chord Builder">Chord Builder</a>
+
+Of course, that will be wrapped in a <li> list item in the menu nav <ul> unordered list.
+
--- app/app.js
+++ app/app.js
@@ -1,93 +1,95 @@
 Pete.ready(function () {
-    var reReplaceHash = /^#/,
+    // Change 'chord_builder' to 'Chord Builder'
+    var reQuizText = /^(.)(.*)(?:_)(.)(.*)$/
+        reReplaceHash = /^#/,
         cachedQuizzes = {};
 
-    Pete.Element.create({
-        tag: 'header',
-        items: [{
-            tag: 'nav',
-            items: [{
-                tag: 'ul',
-                items: [{
+    Pete.ajax.load({
+        url: 'app.json',
+        success: function (response) {
+            var quizzes = JSON ?
+                    JSON.parse(response) :
+                    eval('[' + response + ']'),
+                q = [],
+                i, len, quizText;
+
+            // Construct the quiz menu from app.json.
+            for (i = 0, len = quizzes.length; i < len; i++) {
+                quizText = quizzes[i].replace(reQuizText, function (a, $1, $2, $3, $4) {
+                    return $1.toUpperCase() + $2 + ' ' + $3.toUpperCase() + $4;
+                });
+
+                q.push({
                     tag: 'li',
                     items: [{
                         tag: 'a',
-                        text: 'Chord Puzzle',
+                        text: quizText,
                         attr: {
-                            href: '#chord_puzzle',
-                            title: 'Chord Puzzle'
+                            href: '#' + quizzes[i],
+                            title: quizText
                         }
                     }]
-                }, {
-                    tag: 'li',
-                    items: [{
-                        tag: 'a',
-                        text: 'Chord Builder',
-                        attr: {
-                            href: '#chord_builder',
-                            title: 'Chord Builder'
-                        }
-                    }]
-                }, {
-                    tag: 'li',
-                    items: [{
-                        tag: 'a',
-                        text: 'Key Signature Quiz',
-                        attr: {
-                            href: '#key_signature',
-                            title: 'Key Signature Quiz'
-                        }
-                    }]
-                }]
-            }]
-        }],
-        parent: document.body
-    });
-
-    Pete.Element.gets('nav a').on('click', function (e) {
-        var quiz = e.target.hash.replace(reReplaceHash, ''),
-            iframeId = 'sandbox-' + quiz,
-            dir = 'app/quizzes/' + quiz + '/',
-            sandbox;
-
-        e.preventDefault();
-
-        // Return early if already downloaded.
-        if (cachedQuizzes[quiz]) {
-            return;
-        }
-
-        // Cache the quiz.
-        cachedQuizzes[quiz] = true;
-
-        sandbox = Pete.Element.create({
-            tag: 'iframe',
-            id: iframeId,
-            style: {
-                height: '300px',
-                width: '800px'
-            },
-            parent: document.body
-        }),
-
-        Pete.ajax.load({
-            url: 'app/template.html',
-            success: function (response, request) {
-                var doc = Pete.getDom(iframeId).contentDocument;
-                    tpl = Pete.compose(Pete.Template, {
-                        html: response
-                    });
-
-                doc.open();
-
-                doc.write(tpl.apply({
-                    quiz_css: dir + 'quiz.css',
-                    quiz_js: dir + 'quiz.js'
-                }));
-
-                doc.close();
+                });
             }
-        });
+
+            Pete.Element.create({
+                tag: 'header',
+                items: [{
+                    tag: 'nav',
+                    items: [{
+                        tag: 'ul',
+                        items: q
+                    }]
+                }],
+                parent: document.body
+            });
+
+            Pete.Element.gets('nav a').on('click', function (e) {
+                var quiz = e.target.hash.replace(reReplaceHash, ''),
+                    iframeId = 'sandbox-' + quiz,
+                    dir = 'app/quizzes/' + quiz + '/',
+                    sandbox;
+
+                e.preventDefault();
+
+                // Return early if already downloaded.
+                if (cachedQuizzes[quiz]) {
+                    return;
+                }
+
+                // Cache the quiz.
+                cachedQuizzes[quiz] = true;
+
+                sandbox = Pete.Element.create({
+                    tag: 'iframe',
+                    id: iframeId,
+                    style: {
+                        height: '300px',
+                        width: '800px'
+                    },
+                    parent: document.body
+                }),
+
+                Pete.ajax.load({
+                    url: 'app/template.html',
+                    success: function (response, request) {
+                        var doc = Pete.getDom(iframeId).contentDocument;
+                            tpl = Pete.compose(Pete.Template, {
+                                html: response
+                            });
+
+                        doc.open();
+
+                        doc.write(tpl.apply({
+                            quiz_css: dir + 'quiz.css',
+                            quiz_js: dir + 'quiz.js'
+                        }));
+
+                        doc.close();
+                    }
+                });
+            });
+        }
     });
 });
 
